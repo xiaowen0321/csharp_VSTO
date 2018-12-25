570159d5609980c97fb063d0684b61c4d4c7c8eb
Xiaowen Cao <xw2015@outlook.com>
2018-12-25 21:58:46 +0800

    第一次更改

---
+++ PowerPointAlignCenter/Ribbon1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace PowerPointAlignCenter
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            var selectedShapes = Globals.ThisAddIn.Application.ActiveWindow.Selection.ShapeRange;
+            System.Windows.Forms.MessageBox.Show(selectedShapes.Count.ToString());
+        }
+    }
+}
